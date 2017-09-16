62922a1a2fbd191c2c3268fd5e91c6107583036b
XDEV\omer <omer@techsolutions.net>
2017-09-17 02:35:43 +0500

    Lists UI

--- Sports-Management/WebApp/Controllers/HomeController.cs
+++ Sports-Management/WebApp/Controllers/HomeController.cs
@@ -1,37 +1,13 @@
-﻿using Repository.Pattern;
-using Repository.Pattern.Infrastructure;
-using Repository.Pattern.UnitOfWork;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
-using WebApp.Models;
-using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class HomeController : BaseController
     {
-        
-        
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return RedirectToAction("Index", "Events");
         }
     }
 }
